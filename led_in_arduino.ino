4e8a410094eab37f0e78c695e869b2fed42cfbd2
Gustavo Bizo Jardim <69866294+gbiz0@users.noreply.github.com>
2020-12-07 10:47:47 -0300

    Add files via upload

---
+++ led_in_arduino.ino
@@ -0,0 +1,13 @@
+int pino = 10;
+void setup() {
+  
+  pinMode(pino, OUTPUT);
+}void loop() {
+  
+  digitalWrite(pino, HIGH);
+  
+  delay(1000);
+  
+  digitalWrite(pino, LOW);
+  
+  delay(1000);}
